90d2e29d34f914f8ae0aa48586392a8de798e58c
caiodasilva2005 <caiodasilva05@outlook.com>
2024-11-15 19:54:14 -0500

    urls

--- src/lib/api/exams.ts
+++ src/lib/api/exams.ts
@@ -1,1 +1,14 @@
- 
+import { ExamCreate } from "./types";
+
+const BASE_URL = "/api/exams";
+
+export const examApi = {
+  getAll: () => fetch(BASE_URL).then((res) => res.json()),
+
+  create: (data: ExamCreate) =>
+    fetch(BASE_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    }).then((res) => res.json()),
+};
