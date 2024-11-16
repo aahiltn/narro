2d468165e1852c400fd129f2f274cf8d0b13336d
SwordAndTea <xiangweiqaz@gmail.com>
2024-11-16 10:33:55 -0500

    update hooks by using server action

--- src/hooks/useQuestions.ts
+++ src/hooks/useQuestions.ts
@@ -1,1 +1,43 @@
- 
+import { useState } from "react";
+import { getQuestions, createQuestion } from "@/api/dal/questions";
+import { Question } from "@prisma/client";
+
+interface UseQuestionsReturn {
+  questions: Question[] | null;
+  loading: boolean;
+  error: Error | null;
+  fetchQuestions: () => Promise<void>;
+  newQuestion: (data: Omit<Question, "id">) => Promise<Question | null>;
+}
+
+export function useQuestions(): UseQuestionsReturn {
+  const [questions, setQuestions] = useState<Question[] | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  const fetchQuestions = async () => {
+    try {
+      setLoading(true);
+      const response = await getQuestions();
+      setQuestions(response);
+    } catch (err) {
+      setError(err as Error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const newQuestion = async (data: Omit<Question, "id">) => {
+    try {
+      const response = await createQuestion(data);
+      return response;
+    } catch (err) {
+      setError(err as Error);
+    } finally {
+      setLoading(false);
+    }
+    return null;
+  };
+
+  return { questions, loading, error, fetchQuestions, newQuestion };
+} 
